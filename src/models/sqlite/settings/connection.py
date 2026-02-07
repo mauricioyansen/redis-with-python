a6b2f8e641d941f7eabb9030ed59a69364aa4454
Mauricio <mauricio_cy@hotmail.com>
2026-02-07 17:41:56 -0300

    feat: implement sqlite conn handler

---
+++ src/models/sqlite/settings/connection.py
@@ -0,0 +1,16 @@
+import sqlite3
+from sqlite3 import Connection as SQLiteConn
+
+
+class SqliteConnectionHandle:
+    def __init__(self):
+        self.__connection_string = "storage.db"
+        self.__conn = None
+
+    def connect(self) -> SQLiteConn:
+        conn = sqlite3.connect(self.__connection_string, check_same_thread=False)
+        self.__conn = conn
+        return conn
+
+    def get_connection(self) -> SQLiteConn:
+        return self.__conn
